1a2966c95872b53789a129e484cb7b3885076648
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-28 12:05:03 -0300

    Componente ListaPet renderizado na index.js

--- react-app-pet/src/index.js
+++ react-app-pet/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PetCadastro from './components/petCadastro/PetCadastro';
+import ListaPet from './components/listaPet/ListaPet';
 
 ReactDOM.render(
   <>
-    <PetCadastro />
+    <ListaPet />
   </>,
   document.getElementById('root')
 );
