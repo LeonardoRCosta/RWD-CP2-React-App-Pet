658fba0ddcb2ac0cd08ac8a2070e682913492f84
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-29 09:28:32 -0300

    Chaves dos states padronizadas, falta corrigir o TypeError: listaPets.map is not a function.
    Co-authored-by: Rafael Ramos Costa <RafaRamosCosta@users.noreply.github.com>

--- react-app-pet/src/components/listaPet/ListaPet.jsx
+++ react-app-pet/src/components/listaPet/ListaPet.jsx
@@ -1,64 +1,187 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Pet from '../pet/Pet';
 import PetCadastro from '../petCadastro/PetCadastro';
+import { DivContainer, DivListaPets } from '../../style/styled';
 
 export default function ListaPet() {
-  const [listaPets, setListaPets] = useState([{}])
-  
+  const [listaPets, setListaPets] = useState([
+    {
+      nomePet: 'Almofadinhas',
+      idadePet: '2 anos',
+      racaPet: 'Labrador',
+      tamanhoPet: 'Médio',
+      nomeDono: 'Harry Potter',
+      telDono: '(11) 99999-9999',
+      imgPet: 'sfdasfasf',
+      obs: 'safasdfsa',
+    },
+    {
+      nomePet: 'Rex',
+      idadePet: '1 ano',
+      racaPet: 'Pitbull',
+      tamanhoPet: 'Grande',
+      nomeDono: 'Jão',
+      telDono: '(11) 99999-9999',
+      imgPet: 'fdasfdsa',
+      obs: 'safasdfsa',
+    },
+    {
+      nomePet: 'Rex',
+      idadePet: '1 ano',
+      racaPet: 'Pitbull',
+      tamanhoPet: 'Grande',
+      nomeDono: 'Jão',
+      telDono: '(11) 99999-9999',
+      imgPet: 'fdasfdsa',
+      obs: 'safasdfsa',
+    },
+  ]);
+
   const addPet = (e) => {
     e.preventDefault();
-    setNPet({nome: '', idade: '', raca: '', tamanho: '', nmDono: '', telDono: '', imgPet: '', obs: ''})
-    setListaPets(...listaPets, nPet)
-  }
+    setNovoPet({
+      nomePet: '',
+      idadePet: '',
+      racaPet: '',
+      tamanhoPet: '',
+      nomeDono: '',
+      telDono: '',
+      imgPet: '',
+      obs: '',
+    });
+    setListaPets(...listaPets, novoPet);
+  };
 
-  
-  const [nPet, setNPet] = useState({nome: '', idade: '', raca: '', tamanho: '', nmDono: '', telDono: '', imgPet: '', obs: ''})
+  const [novoPet, setNovoPet] = useState({
+    nomePet: '',
+    idadePet: '',
+    racaPet: '',
+    tamanhoPet: '',
+    nomeDono: '',
+    telDono: '',
+    imgPet: '',
+    obs: '',
+  });
 
   const captura = (e) => {
-    const { name, value } = e.target;
-    if(name === 'nmPet') {
-      setNPet({nome: value, idade: nPet.idade, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: nPet.obs})
+    const {name, value} = e.target;
+
+    if (name === 'nomePet') {
+      setNovoPet({
+        nomePet: value,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'idadePet') {
-      setNPet({nome: nPet.nome, idade: value, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: nPet.obs})
+    if (name === 'idadePet') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: value,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'racaPet') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: value, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: nPet.obs})
+    if (name === 'racaPet') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: value,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'tamanhoPet') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: nPet.raca, tamanho: value, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: nPet.obs})
+    if (name === 'tamanhoPet') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: value,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'nmDono') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: value, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: nPet.obs})
+    if (name === 'nomeDono') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: value,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'telDono') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: value, imgPet: nPet.imgPet, obs: nPet.obs})
+    if (name === 'telDono') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: value,
+        imgPet: novoPet.imgPet,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'imgPet') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: value, obs: nPet.obs})
+    if (name === 'imgPet') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: value,
+        obs: novoPet.obs,
+      });
     }
-    else if(name === 'obs') {
-      setNPet({nome: nPet.nome, idade: nPet.idade, raca: nPet.raca, tamanho: nPet.tamanho, nmDono: nPet.nmDono, telDono: nPet.telDono, imgPet: nPet.imgPet, obs: value})
+    if (name === 'obs') {
+      setNovoPet({
+        nomePet: novoPet.nomePet,
+        idadePet: novoPet.idadePet,
+        racaPet: novoPet.racaPet,
+        tamanhoPet: novoPet.tamanhoPet,
+        nomeDono: novoPet.nomeDono,
+        telDono: novoPet.telDono,
+        imgPet: novoPet.imgPet,
+        obs: value,
+      });
     }
-  }
-  
+  };
+
   return (
-    <div>
-      <PetCadastro addPet={addPet} nPet={nPet} captura={captura}/>
-      {listaPets.map((pet, i) => (
-        
-        <Pet
-        key={i}
-        nome={pet.nome}
-        idade={pet.idade}
-        raca={pet.raca}
-        tamanho={pet.tamanho}
-        nmDono={pet.nmDono}
-        telDono={pet.telDono}
-        imgPet={pet.imgPet}
-        obs={pet.obs}
-        />
-      ))} 
-    </div>
+    <DivContainer>
+      <PetCadastro addPet={addPet} novoPet={novoPet} captura={captura} />
+      <DivListaPets>
+        {listaPets.map((pet, i) => (
+          <Pet
+            key={i}
+            nomePet={pet.nomePet}
+            idadePet={pet.idadePet}
+            racaPet={pet.racaPet}
+            tamanhoPet={pet.tamanhoPet}
+            nomeDono={pet.nomeDono}
+            telDono={pet.telDono}
+            imgPet={pet.imgPet}
+            obs={pet.obs}
+          />
+        ))}
+      </DivListaPets>
+    </DivContainer>
   );
 }
