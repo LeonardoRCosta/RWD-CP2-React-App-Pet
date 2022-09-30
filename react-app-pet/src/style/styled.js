2415cb547a614b7fa9f3022437dd3ac67c500b7f
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-29 23:44:43 -0300

    Estilização do site praticamente concluída. Trabalhar na responsividade. Co-authored-by: Rafael Ramos Costa <RafaRamosCosta@users.noreply.github.com>

--- react-app-pet/src/style/styled.js
+++ react-app-pet/src/style/styled.js
@@ -1,78 +1,37 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
   row-gap: 1.5vh;
+  h1 {
+    background-color: #faaf00;
+    padding: 1vh;
+    border-radius: 5px;
+    box-shadow: -3px 3px 5px 0 rgba(0, 0, 0, 0.5);
+    font-weight: 100;
+  }
   input {
     padding: 1vh 0;
     outline: none;
     width: 30vw;
+    min-width: 200px;
+  }
+  #imgPetId {
+    cursor: pointer;
   }
   textarea {
     width: 30vw;
+    min-width: 200px;
     resize: none;
     outline: none;
   }
 `;
-//--------------------
-export const Botao = styled.button`
-  border-radius: 5px;
-  padding: 1vh 2vw;
-  margin: auto;
-  display: block;
-  border: solid 2px #00ff00;
-  color: #00ff00;
-  font-size: 1.5rem;
-`;
-export const DivForm = styled.div`
-  width: 80vw;
-  padding: 20px;
-  background-color: #ccc;
-
-  form {
-    width: 50%;
-    margin: auto;
-  }
-  form input,
-  form textarea {
-    padding: 5px;
-    width: 80%;
-  }
-  form textarea {
-    height: 80px;
-  }
-  form button {
-    padding: 10px;
-    background: #3c3;
-    color: #fff;
-  }
-`;
-
-export const DivLista = styled.div`
-  width: 100vw;
-  padding: 20px;
-  min-height: 85vh;
-  background-color: #993;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  .btnTarefa {
-    display: block;
-    height: 5vh;
-    margin: auto;
-    border-radius: 5px;
-    padding: 1vh 2vw;
-    border: solid 2px #00ff00;
-    color: #00ff00;
-    font-size: 1.5rem;
-  }
-`;
-//--------------------
 
 export const Button = styled.button`
   border-radius: 2px;
   padding: 1vh 8vw;
+  min-width: 205px;
   width: 30.5vw;
   border: solid px #277355;
   border-radius: 5px;
@@ -84,27 +43,54 @@ export const Button = styled.button`
     color: #000;
     transition: all 0.4s ease-in-out;
     border: solid 2px #000;
+    cursor: pointer;
   }
 `;
 
 export const DivContainer = styled.div`
   display: flex;
   column-gap: 40px;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 80%;
+  }
 `;
 
 export const DivListaPets = styled.div`
   display: flex;
   column-gap: 30px;
   flex-wrap: wrap;
+  @media (max-width: 1000px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export const DivPet = styled.div`
-  background-color: beige;
-  border: 1px solid black;
+  background-color: rgba(255, 255, 128, .5);
+  box-shadow: 3px 3px 5px 0 rgb(0, 0, 0, 0.6);
   border-radius: 5px;
   padding: 10px;
   margin-top: 10px;
-  height: 340px;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-    'Lucida Sans', Arial, sans-serif;
+  height: 450px;
+  width: 300px;
+  figure {
+    display: flex;
+    justify-content: center;
+  }
+  h2 {
+    text-align: center;
+    font-weight: 100;
+  }
+  @media (max-width: 800px) {
+    height: 80%;
+  }
+`;
+
+export const PetImg = styled.img`
+  width: 150px;
+  border-radius: 5px;
 `;
