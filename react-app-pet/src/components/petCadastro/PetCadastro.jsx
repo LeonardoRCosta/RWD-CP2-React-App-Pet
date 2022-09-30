b4a1e926d3270da8998e239ffeaf53e0e76019f6
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-29 23:43:57 -0300

    Finalizado componente PetCadastro.

--- react-app-pet/src/components/petCadastro/PetCadastro.jsx
+++ react-app-pet/src/components/petCadastro/PetCadastro.jsx
@@ -1,33 +1,72 @@
-import React from 'react'
-import { Form, Button} from '../../style/styled';
-
+import React from "react";
+import { Form, Button } from "../../style/styled";
 
 export default function PetCadastro(props) {
-  
   return (
     <div>
-      <h1>Dados do Pet</h1>
-      <Form method='POST' onSubmit={props.addPet}>
+      <Form method="post" onSubmit={props.addPet}>
+        <h1>🦴&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dados do Pet&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;🦴</h1>
         <label htmlFor="nomePet">Nome</label>
-        <input type="text" name="nomePet" value={props.novoPet.nomePet} onChange={props.captura} /> 
+        <input
+          type="text"
+          name="nomePet"
+          value={props.novoPet.nomePet}
+          onChange={props.captura}
+        />
         <label htmlFor="idadePet">Idade</label>
-        <input type="text" name="idadePet" value={props.novoPet.idadePet} onChange={props.captura}/>
+        <input
+          type="text"
+          name="idadePet"
+          value={props.novoPet.idadePet}
+          onChange={props.captura}
+        />
         <label htmlFor="racaPet">Raça</label>
-        <input type="text" name="racaPet" value={props.novoPet.racaPet} onChange={props.captura}/>
+        <input
+          type="text"
+          name="racaPet"
+          value={props.novoPet.racaPet}
+          onChange={props.captura}
+        />
         <label htmlFor="tamanhoPet">Tamanho</label>
-        <input type="text" name="tamanhoPet" value={props.novoPet.tamanhoPet} onChange={props.captura}/>
+        <input
+          type="text"
+          name="tamanhoPet"
+          value={props.novoPet.tamanhoPet}
+          onChange={props.captura}
+        />
         <label htmlFor="nomeDono">Nome do Dono</label>
-        <input type="text" name="nomeDono" value={props.novoPet.nomeDono} onChange={props.captura}/>
+        <input
+          type="text"
+          name="nomeDono"
+          value={props.novoPet.nomeDono}
+          onChange={props.captura}
+        />
         <label htmlFor="telDono">Telefone do Dono</label>
-        <input type="text" name="telDono" value={props.novoPet.telDono} onChange={props.captura}/>
+        <input
+          type="text"
+          name="telDono"
+          value={props.novoPet.telDono}
+          onChange={props.captura}
+        />
         <label htmlFor="imgPet">Imagem do Pet</label>
-        <input type="text" name="imgPet" value={props.novoPet.imgPet} onChange={props.captura}/>
+        <input
+          type="file"
+          accept=".jpg, .gif, .png"
+          name="imgPet"
+          id="imgPetId"
+          value={props.novoPet.imgPet}
+          onChange={props.captura}
+        />
         <label htmlFor="obs">Observações</label>
-        <textarea name="obs" cols="30" rows="10" value={props.novoPet.obs} onChange={props.captura}></textarea>
+        <textarea
+          name="obs"
+          cols="30"
+          rows="10"
+          value={props.novoPet.obs}
+          onChange={props.captura}></textarea>
 
-        <Button type='submit'>Adicionar</Button>
+        <Button type="submit">Adicionar</Button>
       </Form>
-      
     </div>
-  )
+  );
 }
